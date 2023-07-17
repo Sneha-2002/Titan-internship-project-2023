228560695c0509425aff5d8c163b51fd9edbe79c
Sneha kimari <snehaeng2002@gmail.com>
2023-07-17 16:40:34 +0530

    first commit

---
+++ appcasting.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    df=pd.read_excel('data.xlsx')
+
+    table_html = df.to_html(index=False)
+    return render_template('casting.html',table=table_html)
+if __name__ == '__main__':
+    app.run(debug=True)
